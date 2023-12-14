b0cf04e6a6c6abad66a0bfa7495afe96041e2499
Alexandre Amade <133642277+AlexMerenkovDev@users.noreply.github.com>
2023-12-14 17:21:19 +0200

    hello

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello dick!");
+        System.out.println("Hello World!");
+
+        Person pessoa = new Person("Alex", 26);
+        System.out.println("Meu nome e: " + pessoa.getName());
+    }
+}
